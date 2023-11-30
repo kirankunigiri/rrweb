8e45c67905759d2350c63684f3f5c17e695807e6
Justin Halsall <Juice10@users.noreply.github.com>
2023-11-30 16:42:27 +0100

    Check if url is of cacheable origin

--- packages/rrweb/src/replay/index.ts
+++ packages/rrweb/src/replay/index.ts
@@ -1816,17 +1816,7 @@ export class Replayer {
               } else {
                 const targetEl = target as Element | RRElement;
                 targetEl.setAttribute(attributeName, value);
-                if (
-                  this.assetManager.isAttributeCacheable(
-                    targetEl,
-                    attributeName,
-                  )
-                ) {
-                  void this.assetManager.manageAttribute(
-                    targetEl,
-                    attributeName,
-                  );
-                }
+                void this.assetManager.manageAttribute(targetEl, attributeName);
               }
             } catch (error) {
               this.warn(
